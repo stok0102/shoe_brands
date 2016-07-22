136a734957d642083c534d5ee30d0fd37ed4645c
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-22 10:22:56 -0700

    pass titlecase callback spec for store

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,10 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
   validates(:name, :presence => true)
+  before_save :titlecase_name
+
+private
+  def titlecase_name
+    self.name=(name.gsub(/\w+/) { |word| word.capitalize })
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -5,4 +5,9 @@ describe(Store) do
     store = Store.new({:name => ""})
     expect(store.save()).to(eq(false))
   end
+
+  it "titlecases the name entered" do
+    store = Store.create({name: 'doc martens'})
+    expect(store.name).to(eq('Doc Martens'))
+  end
 end

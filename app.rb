1346f472cb186b406ac67591fa3e89364041cf2e
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-22 09:06:53 -0700

    add stores route

---
+++ app.rb
@@ -0,0 +1,25 @@
+require("bundler/setup")
+Bundler.require(:default)
+require('pry')
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get '/' do
+  erb :index
+end
+
+get '/brands' do
+  @brands = Brand.all
+  erb :brands
+end
+
+get '/stores' do
+  @stores = Store.all
+  erb :stores
+end
+
+post '/stores' do
+  name = params.fetch 'name'
+  store = Store.create({name: name})
+  redirect '/stores'
+end

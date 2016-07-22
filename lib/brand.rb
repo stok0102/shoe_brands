2b3e8a39e015d1c7a5e1bcd35a91daad8d2bd081
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-22 10:21:09 -0700

    add callback method to brand to titlecase name

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,10 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
   validates(:name, :presence => true)
-end  
+  before_save :titlecase_name
+
+private
+  def titlecase_name
+    self.name=(name.gsub(/\w+/) { |word| word.capitalize })
+  end
+end
